b81343bcf9ff19a1bc2c765f5586146a73339b12
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2010-07-16 00:08:53 +0000

    + added requantized peak filter (for private use only) + added simple MP3 VST host (for private use only)

---
+++ Examples/Plugins/Filter/Requantized Peak EQ Filter/RpeqGUI.pas
@@ -0,0 +1,16 @@
+unit RpeqGUI;
+
+interface
+
+uses 
+  Windows, Messages, SysUtils, Classes, Forms, DAV_Types, DAV_VSTModule;
+
+type
+  TFmRpeq = class(TForm)
+  end;
+
+implementation
+
+{$R *.DFM}
+
+end.
