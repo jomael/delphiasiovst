0f8fc3c588837566f601a0301ddc565baf7af2c2
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2010-11-11 01:18:44 +0000

    + added pixelmap test

---
+++ trunk/Examples/GUI/PixelMap/PixelMapTest.dpr
@@ -0,0 +1,17 @@
+program PixelMapTest;
+
+uses
+  Forms,
+  BtMain in 'BtMain.pas' {FmPixelMapTest},
+  DAV_GuiPixelMap in '..\..\..\Source\GUI\DAV_GuiPixelMap.pas',
+  DAV_GuiBlend in '..\..\..\Source\GUI\DAV_GuiBlend.pas',
+  DAV_MemoryUtils in '..\..\..\Source\DAV_MemoryUtils.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFmPixelMapTest, FmPixelMapTest);
+  Application.Run;
+end.
