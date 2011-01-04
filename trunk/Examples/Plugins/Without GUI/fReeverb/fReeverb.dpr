56d6ca0a086e8599906d15c533ec680d1f1ec35d
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2011-01-04 14:25:20 +0000

    + added two more plugins without a GUI

---
+++ trunk/Examples/Plugins/Without GUI/fReeverb/fReeverb.dpr
@@ -0,0 +1,30 @@
+{$J-,H+,T-P+,X+,B-,V-,O+,A+,W-,U-,R-,I-,Q-,D-,L-,Y-,C-}
+library fReeverb;
+
+uses
+  FastMM4, // either download the library or comment if there is an error here
+  FastMove, // either download the library or comment if there is an error here
+  madExcept, // either download madExcept or remove mad* if there is an error here
+  madLinkDisAsm,
+  DAV_WinAmp,
+  DAV_VSTEffect,
+  DAV_VSTBasicModule,
+  fReeverbModule in 'fReeverbModule.pas' {fReeverbVST: TVSTModule};
+
+function VstPluginMain(AudioMasterCallback: TAudioMasterCallbackFunc): PVSTEffect; cdecl; export;
+begin
+ Result := VstModuleMain(AudioMasterCallback, TfReeverbVST);
+end;
+
+function WinampDSPGetHeader: PWinAmpDSPHeader; cdecl; export;
+begin
+ Result := WinampDSPModuleHeader(TfReeverbVST);
+end;
+
+exports
+  VstPluginMain name 'main',
+  VstPluginMain name 'VSTPluginMain',
+  WinampDSPGetHeader name 'winampDSPGetHeader2';
+
+begin
+end.
