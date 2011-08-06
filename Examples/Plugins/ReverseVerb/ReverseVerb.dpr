04fd11d1a1850928b9f97ae8e1451e07cb7baa5c
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2011-08-06 12:39:03 +0000

    + added reversed verb plugin

---
+++ Examples/Plugins/ReverseVerb/ReverseVerb.dpr
@@ -0,0 +1,33 @@
+{$J-,H+,T-P+,X+,B-,V-,O+,A+,W-,U-,R-,I-,Q-,D-,L-,Y-,C-}
+library ReverseVerb;
+
+uses
+  FastMM4, // either download the library or comment if there is an error here
+  FastMove, // either download the library or comment if there is an error here
+  madExcept, // either download madExcept or remove mad* if there is an error here
+  madLinkDisAsm,
+  madListProcesses,
+  madListModules,
+  DAV_WinAmp,
+  DAV_VSTEffect,
+  DAV_VSTBasicModule,
+  ReverseVerbVST in 'ReverseVerbVST.pas' {ReverseVerbDataModule: TVSTModule};
+//  ReverseVerbGUI in 'ReverseVerbGUI.pas' {FmReverseVerb};
+
+function VstPluginMain(AudioMasterCallback: TAudioMasterCallbackFunc): PVSTEffect; cdecl; export;
+begin
+ Result := VstModuleMain(AudioMasterCallback, TReverseVerbDataModule);
+end;
+
+function WinampDSPGetHeader: PWinAmpDSPHeader; cdecl; export;
+begin
+ Result := WinampDSPModuleHeader(TReverseVerbDataModule);
+end;
+
+exports
+  VstPluginMain name 'main',
+  VstPluginMain name 'VSTPluginMain',
+  WinampDSPGetHeader name 'winampDSPGetHeader2';
+
+begin
+end.
