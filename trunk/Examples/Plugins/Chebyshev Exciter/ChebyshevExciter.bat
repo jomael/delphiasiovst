0096bb5a6c252fdc55f61b6cc3313dd060ecccb4
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2011-08-21 00:11:46 +0000

    * prepared more plugins for release (& added release scripts)

---
+++ trunk/Examples/Plugins/Chebyshev Exciter/ChebyshevExciter.bat
@@ -0,0 +1,9 @@
+@"..\..\..\Bin\VST-Plugin Unit Test (command line).exe" ..\..\..\Bin\VST\32-Bit\ChebyshevExciter.dll 
+@copy "..\..\..\Bin\VST\32-Bit\ChebyshevExciter.dll" "..\..\..\Bin\VST\32-Bit\Chebyshev Exciter.dll"
+@"..\..\..\Bin\VstPluginScreenshotTool.exe" "..\..\..\Bin\VST\32-Bit\Chebyshev Exciter.dll"
+@move "..\..\..\Bin\VST\32-Bit\Chebyshev Exciter.png" "..\..\..\Screenshots\Chebyshev Exciter.png"
+@7z a "..\..\..\Archive\Chebyshev Exciter.7z" "..\..\..\Bin\VST\*\Chebyshev Exciter.dll" "..\..\..\Manuals\Chebyshev Exciter.pdf" "..\..\..\Bin\License.txt"
+@makensis /V2 "..\..\..\Install Scripts\Install Script Chebyshev Exciter.nsi"
+@ftps -s:"..\..\..\Release Scripts\Chebyshev Exciter.ftp"
+@WinSCP -script="..\..\..\Release Scripts\Chebyshev Exciter.scp"
+pause
