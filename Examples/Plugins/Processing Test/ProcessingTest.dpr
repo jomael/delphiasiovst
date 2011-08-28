fbd3692c565cb5fb3c01ef47937a078128afdf49
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2011-08-28 12:59:35 +0000

    + added a simple plugin to test whether 32-Bit or 64-Bit processing is used

---
+++ Examples/Plugins/Processing Test/ProcessingTest.dpr
@@ -0,0 +1,26 @@
+{$J-,H+,T-P+,X+,B-,V-,O+,A+,W-,U-,R-,I-,Q-,D-,L-,Y-,C-}
+library ProcessingTest;
+
+uses
+  DAV_WinAmp,
+  DAV_VSTEffect,
+  DAV_VSTBasicModule,
+  PluginVST in 'PluginVST.pas' {ProcessingTestModule: TVSTModule};
+
+function VstPluginMain(AudioMasterCallback: TAudioMasterCallbackFunc): PVSTEffect; cdecl; export;
+begin
+  Result := VstModuleMain(AudioMasterCallback, TProcessingTestModule);
+end;
+
+function WinampDSPGetHeader: PWinAmpDSPHeader; cdecl; export;
+begin
+  Result := WinampDSPModuleHeader(TProcessingTestModule);
+end;
+
+exports
+  VstPluginMain name 'main',
+  VstPluginMain name 'VSTPluginMain',
+  WinampDSPGetHeader name 'winampDSPGetHeader2';
+
+begin
+end.
