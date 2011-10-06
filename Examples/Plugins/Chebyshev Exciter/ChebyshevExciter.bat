e97ac979dab84da58adaa31f61f235ee74e37c84
CWBudde <CWBudde@885d5200-cde2-4488-8a86-08278623c11c>
2011-10-06 15:55:16 +0000

    * further changes for Delphi XE2 and 64-Bit support

--- Examples/Plugins/Chebyshev Exciter/ChebyshevExciter.bat
+++ Examples/Plugins/Chebyshev Exciter/ChebyshevExciter.bat
@@ -1,8 +1,10 @@
-@"..\..\..\Bin\VST-Plugin Unit Test (command line).exe" ..\..\..\Bin\Win32\VST\ChebyshevExciter.dll 
-@copy "..\..\..\Bin\Win32\VST\ChebyshevExciter.dll" "..\..\..\Bin\Win32\VST\Chebyshev Exciter.dll"
-@"..\..\..\Bin\VstPluginScreenshotTool.exe" "..\..\..\Bin\Win32\VST\Chebyshev Exciter.dll"
-@move "..\..\..\Bin\Win32\VST\Chebyshev Exciter.png" "..\..\..\Screenshots\Chebyshev Exciter.png"
-@7z a "..\..\..\Archive\ChebyshevExciter.7z" "..\..\..\Bin\*\VST\Chebyshev Exciter.dll" "..\..\..\Manuals\Chebyshev Exciter.pdf" "..\..\..\Bin\License.txt"
+@"..\..\..\Bin\Win32\madExceptPatch.exe" "..\..\..\Bin\Win32\VST\ChebyshevExciter.dll " .\ChebyshevExciter.mes
+@move "..\..\..\Bin\Win32\VST\ChebyshevExciter.dll" "..\..\..\Bin\Win32\VST\Chebyshev Exciter.dll"
+@move "..\..\..\Bin\Win64\VST\ChebyshevExciter.dll" "..\..\..\Bin\Win64\VST\Chebyshev Exciter.dll"
+@"..\..\..\Bin\Win32\VST-Plugin Unit Test (command line).exe" "..\..\..\Bin\Win32\VST\Chebyshev Exciter.dll"
+@"..\..\..\Bin\Win64\VST-Plugin Unit Test (command line).exe" "..\..\..\Bin\Win64\VST\Chebyshev Exciter.dll"
+@"..\..\..\Bin\Win32\VST-Plugin Screenshot Tool.exe" "..\..\..\Bin\Win32\VST\Chebyshev Exciter.dll" "..\..\..\Screenshots\Chebyshev Exciter.png"
+@7z a "..\..\..\Archive\Chebyshev Exciter.7z" "..\..\..\Bin\*\VST\Chebyshev Exciter.dll" "..\..\..\Manuals\Chebyshev Exciter.pdf" "..\..\..\Bin\License.txt"
 @makensis /V2 "..\..\..\Install Scripts\Install Script Chebyshev Exciter.nsi"
 @ftps -s:"..\..\..\Release Scripts\Chebyshev Exciter.ftp"
 @WinSCP -script="..\..\..\Release Scripts\Chebyshev Exciter.scp"
--- Examples/Plugins/Chebyshev Waveshaper/ChebyshevWaveshaper.bat
+++ Examples/Plugins/Chebyshev Waveshaper/ChebyshevWaveshaper.bat
@@ -1,9 +1,11 @@
-@copy "..\..\..\Bin\Win32\VST\ChebyshevWaveshaper.dll" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll"
-@"..\..\..\Bin\VST-Plugin Unit Test (command line).exe" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll"
-@"..\..\..\Bin\VstPluginScreenshotTool.exe" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll"
-@move "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.png" "..\..\..\Screenshots\Chebyshev Waveshaper.png"
-@7z a "..\..\..\Archive\ChebyshevWaveshaper.7z" "..\..\..\Bin\*\VST\Chebyshev Waveshaper.dll" "..\..\..\Bin\License.txt"
+@"..\..\..\Bin\Win32\madExceptPatch.exe" "..\..\..\Bin\Win32\VST\ChebyshevWaveshaper.dll " .\ChebyshevWaveshaper.mes
+@move "..\..\..\Bin\Win32\VST\ChebyshevWaveshaper.dll" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll"
+@move "..\..\..\Bin\Win64\VST\ChebyshevWaveshaper.dll" "..\..\..\Bin\Win64\VST\Chebyshev Waveshaper.dll"
+@"..\..\..\Bin\Win32\VST-Plugin Unit Test (command line).exe" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll"
+@"..\..\..\Bin\Win64\VST-Plugin Unit Test (command line).exe" "..\..\..\Bin\Win64\VST\Chebyshev Waveshaper.dll"
+@"..\..\..\Bin\Win32\VST-Plugin Screenshot Tool.exe" "..\..\..\Bin\Win32\VST\Chebyshev Waveshaper.dll" "..\..\..\Screenshots\Chebyshev Waveshaper.png"
+@7z a "..\..\..\Archive\Chebyshev Waveshaper.7z" "..\..\..\Bin\*\VST\Chebyshev Waveshaper.dll" "..\..\..\Manuals\Chebyshev Waveshaper.pdf" "..\..\..\Bin\License.txt"
 @makensis /V2 "..\..\..\Install Scripts\Install Script Chebyshev Waveshaper.nsi"
 @ftps -s:"..\..\..\Release Scripts\Chebyshev Waveshaper.ftp"
 @WinSCP -script="..\..\..\Release Scripts\Chebyshev Waveshaper.scp"
-@Pause
+pause
